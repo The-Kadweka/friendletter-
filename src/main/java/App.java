9a254b4292a93f36393f3883d523489c24573a8b
hussein18149 <husseinkatana17@gmail.com>
2020-01-28 01:39:44 +0300

    download gradle and build app then run application successfully

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -2,6 +2,6 @@ import static spark.Spark.*;
 
 public class App{
     public static void main(String[] args){
-        get("/hello", (request, response) -> "Hello Friend!");
+         get("/hello", (request, response) -> "Hello Friend!");
     }
 }
